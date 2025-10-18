3b9dc7e7a0460478b15cd5c8bcc8d349eb7c0bff
MarkEmad656 <61595150+mark701@users.noreply.github.com>
2025-10-18 21:47:52 +0300

    Update ApiUrl.jsx

--- src/utils/ApiUrl.jsx
+++ src/utils/ApiUrl.jsx
@@ -1,3 +1,3 @@
-// export const API_BASE_URL =  "https://markemad-001-site1.stempurl.com/";
+export const API_BASE_URL =  "https://markemad-001-site1.stempurl.com/";
 
- export const API_BASE_URL =  "https://localhost:7028/";
+//  export const API_BASE_URL =  "https://localhost:7028/";
